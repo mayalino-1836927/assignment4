aed30af11390560ebd3e5ae63c3220bc661035e6
mayalino-1836927 <43831948+mayalino-1836927@users.noreply.github.com>
2020-12-02 21:17:53 -0800

    Add files via upload

--- app.R
+++ app.R
@@ -13,7 +13,7 @@ data_range <- range(climate_data$year)
 
 select_values <- colnames(climate_data)
 
-price_input <- sliderInput(
+co2_input <- sliderInput(
   inputId = "co2_choice",
   label = "CO2 range",
   min = data_range[1],
@@ -33,11 +33,29 @@ ui <- fluidPage(
     "Climate Change Analysis",
     tabPanel(
       "Summary Data and Analysis",
-      h4("This project seeks to visualize CO2 data collected by the Our World in Data project; "),
+      h3("This project seeks to visualize CO2 data collected by the Our World in Data project. 
+         Additionally for this project, are the computed values of the highest co2 producers and consumers, 
+         as well the GDP of those countries. Additional data columns have also been added into the dataset 
+         answering such questions as: What is the ratio of energy per capity by the energy per gdp? What is the 
+         growth of CO2 by GDP? What is the energy per GDP by population? What is the energy per capita by poulation? 
+         What is the primary energy consumption by population? and What is the primary energy consumption by GDP?"),
+      h4("The analysis shows that : "),
+      h4("the highest consumers of CO2 are ", highest_co2_consumer),
+      h4("and its GDP is ", highest_co2_consumer_gdp),
+      h4("the highest producers of CO2 are ", highest_co2_producer),
+      h4("and its GDP is ", highest_co2_producer_gdp),
+      h4("the highest consumers of CO2 are ", lowest_co2_consumer),
+      h4("and its GDP is ", lowest_co2_consumer_gdp),
+      h4("the highest producers of CO2 are ", lowest_co2_producer),
+      h4("and its GDP is ", lowest_co2_producer_gdp),
+      h3("Particularly, the aim of these calculations is to show the disparity of CO2 consumption and production by 
+         wealth. There is an imbalance between the wealthier imperialist countries and its neo-colonies 
+         which suffer the most from global warming yet contribute the least to it and garner no benefits from CO2 
+         consumption/production when compared with themore affluent countries.")
     ),
     tabPanel(
       "CO2 Viewer",
-      price_input,
+      co2_input,
       feature_input,
       checkboxInput("smooth", label = strong("Show Trendline"), value = TRUE),
       plotOutput("plot")
--- app_server.R
+++ app_server.R
@@ -20,5 +20,59 @@ climate_data <- read.csv("https://raw.githubusercontent.com/owid/co2-data/master
     energy_gdp_by_pop = energy_per_gdp / population,
     energy_capita_by_pop = energy_per_capita / population,
     prim_energy_consum_by_pop  = primary_energy_consumption / population,
-    prim_energy_consum_by_gdp  = primary_energy_consumption / population
+    prim_energy_consum_by_gdp  = primary_energy_consumption / gdp
   )
+
+highest_co2_producer <- climate_data %>%
+  group_by(country) %>%
+  summarize(cumulative_co2 = mean(co2, na.rm = TRUE)) %>%
+  filter(cumulative_co2 == max(cumulative_co2)) %>%
+  select(country) %>%
+  print()
+  #group_by(country) %>%
+  #filter(cumulative_co2 > 0) %>%
+  #summarize(num_co2 = n()) %>%
+  #filter(num_co2 == max(num_co2)) %>%
+  #select(country) %>%
+  #print()
+  
+lowest_co2_producer <- climate_data %>%
+  group_by(country) %>%
+  summarize(cumulative_co2 = mean(co2, na.rm = TRUE)) %>%
+  filter(cumulative_co2 == min(cumulative_co2)) %>%
+  select(country) %>%
+  print()
+
+highest_co2_consumer <- climate_data %>%
+  group_by(country) %>%
+  summarize(consumption_co2 = mean(co2, na.rm = TRUE)) %>%
+  filter(consumption_co2 == min(consumption_co2)) %>%
+  select(country) %>%
+  print()
+
+lowest_co2_consumer <- climate_data %>%
+  group_by(country) %>%
+  summarize(consumption_co2 = mean(co2, na.rm = TRUE)) %>%
+  filter(consumption_co2 == max(consumption_co2)) %>%
+  select(country) %>%
+  print()
+
+highest_co2_producer_gdp <- climate_data %>%
+  filter(country == highest_co2_producer) %>%
+  select(gdp) %>%
+  print()
+
+lowest_co2_producer_gdp <- climate_data %>%
+  filter(country == lowest_co2_producer) %>%
+  select(gdp) %>%
+  print()
+
+highest_co2_consumer_gdp <- climate_data %>%
+filter(country == highest_co2_consumer) %>%
+  select(gdp) %>%
+  print()
+
+lowest_co2_consumer_gdp <- climate_data %>%
+filter(country == lowest_co2_consumer) %>%
+  select(gdp) %>%
+  print()
